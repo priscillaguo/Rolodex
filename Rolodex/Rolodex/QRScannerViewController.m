ebeca5f266b8434e25b30f2b5a696c8f886cd826
Sydney Wang <sydneyw@sydneyw-mba.dhcp.thefacebook.com>
2015-06-29 16:46:49 -0700

    TJ DID SOME THINGS. MAYBE THIS WORKS

--- Rolodex/Rolodex/QRScannerViewController.m
+++ Rolodex/Rolodex/QRScannerViewController.m
@@ -8,6 +8,104 @@
 
 #import "QRScannerViewController.h"
 
+@interface QRScannerViewController ()
+
+@property (nonatomic) BOOL isReading;
+//Is NO when not reading a QRCode
+
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+-(BOOL)startReading;
+-(void)stopReading;
+
+@end
+
 @implementation QRScannerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _isReading = NO;
+    _captureSession = nil;
+}
+
+- (BOOL)startReading {
+    NSError *error;
+    
+    //Use camera to capture QRCode
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    if (!input) {
+        NSLog(@"%@", [error localizedDescription]);
+        return NO;
+    }
+    
+    _captureSession = [[AVCaptureSession alloc] init];
+    [_captureSession addInput:input];
+    
+    AVCaptureMetadataOutput *captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [_captureSession addOutput:captureMetadataOutput];
+    
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetadataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeQRCode]];
+    
+    //Show the user what the camera sees
+    _videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    [_videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [_videoPreviewLayer setFrame:_viewPreview.layer.bounds];
+    [_viewPreview.layer addSublayer:_videoPreviewLayer];
+    
+    //Let the session run
+    [_captureSession startRunning];
+    
+    return YES;
+}
+
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    
+    //The NSArray object contains all the metadata (data contained in a QR code)
+    //So must make sure this is not nil and contains an object to proceed:
+    if (metadataObjects != nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeQRCode]) {
+            [_lblStatus performSelectorOnMainThread:@selector(setText:) withObject:[metadataObj stringValue] waitUntilDone:NO];
+            
+            [self performSelectorOnMainThread:@selector(stopReading) withObject:nil waitUntilDone:NO];
+            [_bbitemStart performSelectorOnMainThread:@selector(setTitle:) withObject:@"Start!" waitUntilDone:NO];
+            _isReading = NO;
+        }
+    }
+}
+
+-(void)stopReading{
+    [_captureSession stopRunning];
+    _captureSession = nil;
+    
+    [_videoPreviewLayer removeFromSuperlayer];
+}
+
+
+- (IBAction)startStopReading:(id)sender {
+    
+    //If not already reading a QRCode, do so:
+    if (!_isReading) {
+        if ([self startReading]) {
+            [_bbitemStart setTitle:@"Stop"];
+            [_lblStatus setText:@"Scanning for QR Code..."];
+        }
+    }
+    else{
+        [self stopReading];
+        [_bbitemStart setTitle:@"Start!"];
+    }
+    
+    _isReading = !_isReading;
+}
 
 @end
