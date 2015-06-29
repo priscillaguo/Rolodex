8192e6c048e3683cb0b2410965f89cb4bf80628e
Amanda Glazer <amandaglazer@amandaglaze-mbp.dhcp.thefacebook.com>
2015-06-29 13:51:21 -0700

    Deleted unnecessary librarires.  Added QRScannerView file.

---
+++ Rolodex/Rolodex/QRScannerViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRScannerViewController.h
+//  Rolodex
+//
+//  Created by Amanda Glazer on 6/29/15.
+//  Copyright (c) 2015 Rolodex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRScannerViewController : UIViewController 
+
+@end
---
+++ Rolodex/Rolodex/QRScannerViewController.m
@@ -0,0 +1,13 @@
+//
+//  QRScannerViewController.m
+//  Rolodex
+//
+//  Created by Amanda Glazer on 6/29/15.
+//  Copyright (c) 2015 Rolodex. All rights reserved.
+//
+
+#import "QRScannerViewController.h"
+
+@implementation QRScannerViewController
+
+@end
